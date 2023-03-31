34a425b07d18984cb6742850b773fa0d986059cd
nutzuma <61080215+Nutzerza@users.noreply.github.com>
2023-03-31 16:02:54 +0700

    Update ProbHash.py

--- ProbHash.py
+++ ProbHash.py
@@ -4,47 +4,27 @@ class ProbHash:
         self.hashtable = cap*[None]
         self.n = cap
     def hashFunction(self, mykey):
-        if self.hashtable[mykey%self.n] != None:
-            self.hashtable[mykey%self.n]   
-            return True  
+        pos = mykey%self.n
+        if self.hashtable[pos] == None:
+            return pos
         else:
-            return False
-
+            return self.hashtable[pos]
     def rehashFunction(self, hashkey):
         num = 0
-        while self.hashFunction(hashkey) == False:
+        while type(self.hashFunction(hashkey)) != int:
             hashkey += 1
             num += 1
             if num == self.n:
                 return False
-        return True
-
+        return hashkey%self.n
     def insertData(self, student_obj):
         id = student_obj.getId()
-        canInsert = True
-        if self.hashFunction(id) == False:
-            if self.rehashFunction(id) == False:
-                canInsert = False
-        if canInsert:
-            print("Insert", student_obj.getId(), "at index")
+        pos = self.rehashFunction(id)
+        if pos!=False:
+            self.hashtable[pos] = student_obj
+            print("Insert", student_obj.getId(), "at index", pos)
         else:
             print("Can't not insert", student_obj.getId())
     def searchData(self, key):
-        # ดักไว้ว่ารันครบเท่ากับก็คือตันแล้ว
-        check = key%self.n #
-        have = False
-        fir = check#
-        while self.hashFunction(check):
-            if self.hashtable[check].getId() == key:
-                have = True
-                break
-            check += 1#
-            if check == self.n:
-                check = 0
-            if fir == check:
-                break
-        if have:
-            print("Found", key, "at index", check)
-            return self.hashtable[check]
-        else:
-            print(key, "does not exist.")
+        pos = self.rehashFunction(key)
+        return self.hashtable[pos]
